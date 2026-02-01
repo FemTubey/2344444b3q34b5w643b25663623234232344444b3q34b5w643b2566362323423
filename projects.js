0f1c3317be618a15078ea139cf46ef8662375128
FemTubey <161897656+FemTubey@users.noreply.github.com>
2026-02-01 06:36:18 -0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -2,19 +2,31 @@ const projects = [
     {
         title: "TCarry",
         description: "TCarry is made easy to config. This resource was made in the standalone framework",
-        github: "https://github.com/dummy3344434/TCarry"
+        github: "https://github.com/FemTubey/TCarry/releases/download/V0.0.1/TCarry.zip"
     },
 
     {
         title: "TEngine",
         description: "TEngine was made easy to config. This resource was made in the standalone framework",
-        github: "https://github.com/dummy3344434/TEngine/releases/tag/V0.0.2"
+        github: "https://github.com/FemTubey/TEngine/releases/download/V0.0.2/TEngine.zip"
     },
 
     {
         title: "TEConomy",
         description: "Simple Economy system for fivem. This resource was made in the standalone framework",
-        github: "https://github.com/dummy3344434/TEConomy/releases/tag/V0.0.1"
+        github: "https://github.com/FemTubey/TEConomy/releases/download/V0.0.1/TEConomy.zip"
+    },
+
+    {
+        title: "TloadingScreen",
+        description: "Loadingscreen with preset audio, video background. This resource was made in the standalone framework",
+        github: "https://github.com/FemTubey/TLoadingScreen/releases/download/V0.0.1/TLoadingscreen.zip"
+    },
+
+    {
+        title: "TAfk",
+        description: "Simple Afk disconnect resource. This resource was made in the standalone framework",
+        github: "https://github.com/FemTubey/TAfk/releases/download/V0.0.1/TAfk.zip"
     }
 ];
 
